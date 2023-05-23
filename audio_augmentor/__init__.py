7de65781c90f4492b2f566838d645cb88a860ab3
josebeo2016 <josebeo2016@gmail.com>
2023-05-23 22:48:11 +0900

    test no need import

--- audio_augmentor/__init__.py
+++ audio_augmentor/__init__.py
@@ -1,10 +1,11 @@
-from .background_noise import BackgroundNoiseAugmentor
-from .pitch import PitchAugmentor
-from .reverb import ReverbAugmentor
-from .speed import SpeedAugmentor
-from .volume import VolumeAugmentor
+# from audio_augmentor.background_noise import BackgroundNoiseAugmentor
+# from audio_augmentor.pitch import PitchAugmentor
+# from audio_augmentor.reverb import ReverbAugmentor
+# from audio_augmentor.speed import SpeedAugmentor
+# from audio_augmentor.volume import VolumeAugmentor
+# from audio_augmentor import adversarial
 
-from . import utils
+# from . import utils
 
 from .__version__ import (
     
