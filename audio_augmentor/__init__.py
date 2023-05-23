6e9e60fbaff0ac71bafe8713b759ec974149830f
josebeo2016 <josebeo2016@gmail.com>
2023-05-23 22:50:14 +0900

    test import full name

--- audio_augmentor/__init__.py
+++ audio_augmentor/__init__.py
@@ -1,8 +1,8 @@
-# from audio_augmentor.background_noise import BackgroundNoiseAugmentor
-# from audio_augmentor.pitch import PitchAugmentor
-# from audio_augmentor.reverb import ReverbAugmentor
-# from audio_augmentor.speed import SpeedAugmentor
-# from audio_augmentor.volume import VolumeAugmentor
+from audio_augmentor.background_noise import BackgroundNoiseAugmentor
+from audio_augmentor.pitch import PitchAugmentor
+from audio_augmentor.reverb import ReverbAugmentor
+from audio_augmentor.speed import SpeedAugmentor
+from audio_augmentor.volume import VolumeAugmentor
 # from audio_augmentor import adversarial
 
 # from . import utils
