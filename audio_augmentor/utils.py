87785a7e2502b8f8f0b5adc27a19f0b13cfe341b
josebeo2016 <josebeo2016@gmail.com>
2023-05-26 13:12:44 +0900

    add run_cmd

--- audio_augmentor/utils.py
+++ audio_augmentor/utils.py
@@ -2,6 +2,10 @@ import os
 import numpy as np
 import librosa  
 from pydub import AudioSegment
+import subprocess
+
+import logging
+logger = logging.getLogger(__name__)
 
 def recursive_list_files(path, file_type=["wav", "mp3", "flac"]):
     """Recursively lists all files in a directory and its subdirectories"""
@@ -24,4 +28,39 @@ def librosa_to_pydub(audio_data, sr=16000):
                     frame_rate=sr,
                     sample_width=audio_data.dtype.itemsize, 
                     channels=1)
+
+def run_cmd(cmd):
+    try:
+        output = subprocess.call(cmd, shell=True, stderr=subprocess.STDOUT, stdout=subprocess.DEVNULL)
+    except subprocess.CalledProcessError as e:
+        output = e.output
+    return output.decode()
+
+def down_load_model(model_name, save_path):
+    """
+    This function help to download the pretrained model of famous ASV spoofing models
     
+    :param model_name: the name of the model. Support ["rawnet2", "aasistssl", "xlsr2_300m"]
+    :param save_path: the path to save the pretrained model
+    """
+    if model_name == "rawnet2":
+        if os.path.exists(os.path.join(save_path,"pre_trained_DF_RawNet2.pth")):
+            return
+        logger.info("Downloading pretrained model for Rawnet2")
+        run_cmd("wget https://www.asvspoof.org/asvspoof2021/pre_trained_DF_RawNet2.zip")
+        run_cmd("unzip pre_trained_DF_RawNet2.zip")
+        run_cmd("rm pre_trained_DF_RawNet2.zip")
+        run_cmd("mv pre_trained_DF_RawNet2.pth {}".format(os.path.join(save_path,"pre_trained_DF_RawNet2.pth")))
+    if model_name == "aasistssl":
+        if os.path.exists(os.path.join(save_path,"LA_model.pth")):
+            return
+        logger.info("Downloading pretrained model for AASIST-SSL")
+        run_cmd("gdown 11vFBNKzYUtWqdK358_JEygawFzmmaZjO")
+        run_cmd("mv LA_model.pth {}".format(os.path.join(save_path,"LA_model.pth")))
+    
+    if model_name == "xlsr2_300m":
+        if os.path.exists(os.path.join(save_path,"xlsr2_300m.pth")):
+            return
+        logger.info("Downloading pretrained model for Wav2Vec2.0 (XLSR-2.0-300M)")
+        run_cmd("wget https://dl.fbaipublicfiles.com/fairseq/wav2vec/xlsr2_300m.pt")
+        run_cmd("mv xlsr2_300m.pt {}".format(os.path.join(save_path,"xlsr2_300m.pth")))
